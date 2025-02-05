cb752ac68c33204a4bdc77a487d2a3e60041babe
Anton Parfonov <antonparfonov@gmail.com>
2025-02-05 02:23:43 +0200

    basic web-server/main

--- crates/bin/web-server/src/main.rs
+++ crates/bin/web-server/src/main.rs
@@ -1,4 +1,11 @@
+use axum::{Router, routing};
+use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
     bin_common::init();
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let router = Router::new().route("/healthcheck", routing::get(|| async { "OK" }));
+    axum::serve(listener, router).await.unwrap();
 }
