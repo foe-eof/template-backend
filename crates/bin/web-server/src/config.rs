0c08f264641c5a4741fcfa3235d05642f53bc2db
Anton Parfonov <antonparfonov@gmail.com>
2025-02-06 11:53:26 +0200

    use verdict

--- crates/bin/web-server/src/config.rs
+++ crates/bin/web-server/src/config.rs
@@ -7,7 +7,7 @@ pub struct Config {
     pub socket_addr: SocketAddr,
 }
 
-impl cfg::Config for Config {
+impl verdict::Config for Config {
     const DEFAULT_CONFIG_DIR: &'static str = "./config/web-server";
     const ENV_PREFIX: &'static str = "WEB_SERVER";
 }
